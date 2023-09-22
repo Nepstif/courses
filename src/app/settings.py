daa9db370589e0937435d47f84bfab5814da4f18
Ratmir Ishmuratov <nepstif@gmail.com>
2023-09-22 17:50:07 +0500

    created the frame

--- src/app/settings.py
+++ src/app/settings.py
@@ -103,13 +103,17 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ru-Ru'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = "Europe/Moscow"
 
 USE_I18N = True
 
+USE_L10N = True
+
 USE_TZ = True
+
+DATE_FORMAT = "d E Y"
 
 
 # Static files (CSS, JavaScript, Images)
